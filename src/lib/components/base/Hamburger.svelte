aa7b4b4b2f54298e8bb3cd44f063c46aeaaa150b
Dennis Lee <dennis.lee@gmx.ch>
2025-02-24 17:15:27 +0100

    refactor: hmburger with snippets

--- src/lib/components/base/Hamburger.svelte
+++ src/lib/components/base/Hamburger.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	let { isOpen = $bindable() } = $props();
-
-	let itemClass = 'w-6 h-1 bg-slate-500 rounded-sm transition-all ease-out duration-300 block';
 </script>
 
-<!-- svelte-ignore a11y_consider_explicit_label -->
-<button onclick={() => isOpen = !isOpen} class="h-1/6">
+{#snippet item(anim: string)}
+	<span class="block h-1 w-6 rounded-sm bg-slate-500 transition-all duration-300 ease-out {anim}"
+	></span>
+{/snippet}
+
+<button onclick={() => (isOpen = !isOpen)} class="h-1/6">
 	<div class="flex flex-col items-center justify-center">
-		<span class="{itemClass} {isOpen ? 'translate-y-1 rotate-45' : '-translate-y-0.5'}"></span>
-		<span class="{itemClass} {isOpen ? 'opacity-0' : 'opacity-100'}"></span>
-		<span class="{itemClass} {isOpen ? '-translate-y-1 -rotate-45' : 'translate-y-0.5'}"></span>
+		{@render item(isOpen ? 'translate-y-1 rotate-45' : '-translate-y-0.5')}
+		{@render item(isOpen ? 'opacity-0' : 'opacity-100')}
+		{@render item(isOpen ? '-translate-y-1 -rotate-45' : 'translate-y-0.5')}
 	</div>
 </button>
