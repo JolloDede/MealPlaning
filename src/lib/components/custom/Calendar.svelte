6f6cc083530b0a03766ff501db99cb41ee1f3bf3
Dennis Lee <dennis.lee@gmx.ch>
2025-03-08 21:24:37 +0100

    refactor: calendar prop dates

--- src/lib/components/custom/Calendar.svelte
+++ src/lib/components/custom/Calendar.svelte
@@ -1,33 +1,45 @@
-<script lang='ts'>
-	import { GetCalendarMonth, SameDate, SameWeek } from "$lib/utils";
-	import CalendarDay from "./CalendarDay.svelte";
-    let today = new Date();
+<script lang="ts">
+	import { GetCalendarMonth, SameDate, SameWeek } from '$lib/utils';
+	import CalendarDay from './CalendarDay.svelte';
 
-    let days = GetCalendarMonth(today);
+	interface Props {
+		days: Date[];
+        selStartDate: Date;
+        selEndDate: Date;
+	}
+
+	let { days, selStartDate, selEndDate }: Props = $props();
+
+	let today = new Date();
 </script>
 
-<div id="cal" class="grid grid-cols-7 select-none text-right mx-auto w-fit">
-    <p>Mo</p>
-    <p>Di</p>
-    <p>Mi</p>
-    <p>Do</p>
-    <p>Fr</p>
-    <p>Sa</p>
-    <p>So</p>
+<div id="cal" class="mx-auto grid w-fit select-none grid-cols-7 text-right">
+	<p>Mo</p>
+	<p>Di</p>
+	<p>Mi</p>
+	<p>Do</p>
+	<p>Fr</p>
+	<p>Sa</p>
+	<p>So</p>
 
-    {#each days as day}
-        <CalendarDay currentMonth={day.getMonth() == today.getMonth()} selected={SameWeek(today, day)} selStart={today.getDate() - today.getDay() + 1 == day.getDate()} selEnd={today.getDate() + 7 - today.getDay() == day.getDate()}>
-            <p>{day.getDate()}</p>
-        </CalendarDay>
-    {/each}
+	{#each days as day}
+		<CalendarDay
+			currentMonth={day.getMonth() == today.getMonth()}
+			selected={SameWeek(today, day)}
+			selStart={selStartDate.getDate() == day.getDate()}
+			selEnd={selEndDate.getDate() == day.getDate()}
+		>
+			<p>{day.getDate()}</p>
+		</CalendarDay>
+	{/each}
 </div>
 
 <style>
-    #cal > * {
-        padding: 4px;
-    }
+	#cal > * {
+		padding: 4px;
+	}
 
-    #cal p {
-        text-align: center;
-    }
+	#cal p {
+		text-align: center;
+	}
 </style>
--- src/routes/home/calendar/+page.svelte
+++ src/routes/home/calendar/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import Calendar from "$lib/components/custom/Calendar.svelte";
+	import { GetCalendarMonth } from "$lib/utils";
+
+	let today = new Date();
+
+    let days = GetCalendarMonth(today);
+	let selStartDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - today.getDay() + 1);
+	let selEndDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7 - today.getDay());
 </script>
 
 <div class="px-8">
-	<Calendar />
+	<Calendar days={days} selStartDate={selStartDate} selEndDate={selEndDate} />
 </div>
