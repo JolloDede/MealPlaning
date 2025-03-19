365537d43d657cfa20ea1cf81aa366629a34462c
Dennis Lee <dennis.lee@gmx.ch>
2025-03-19 18:52:32 +0100

    style: sshadow

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div class="flex max-h-screen flex-col">
-	<header class="ham flex items-center">
+	<header class="ham flex items-center shadow-md">
 		<div class="flex w-full flex-row px-3">
 			<!-- svelte-ignore a11y_missing_content -->
 			<h1 class="flex-grow"></h1>
